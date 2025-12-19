55444bf8b98c27badf10c27020c523e5448e3afa
ArthurBlanchon <112695921+ArthurBlanchon@users.noreply.github.com>
2025-12-19 14:37:54 +0100

    Bypassed /home to /about

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/about',
+        permanent: true,
+      },
+    ]
+  },
 }
